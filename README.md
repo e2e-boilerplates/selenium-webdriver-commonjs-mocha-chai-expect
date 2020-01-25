e1f0095082016b37be9415a69e88daed2ec3240c
Girma Nigusse <xgirma@gmail.com>
2020-01-25 16:36:52 -0500

    update tests

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # selenium-webdriver (commonjs, mocha, chai)
 
-[![GitHub Actions status | e2e-boilerplates/selenium-webdriver-commonjs-mocha-chai](https://github.com/e2e-boilerplates/selenium-webdriver-commonjs-mocha-chai/workflows/NodeCI/badge.svg)](https://github.com/e2e-boilerplates/selenium-webdriver-commonjs-mocha-chai/actions?workflow=NodeCI)
+[![GitHub Actions status | e2e-boilerplates/selenium-webdriver-commonjs-mocha-chai](https://github.com/e2e-boilerplates/selenium-webdriver-commonjs-mocha-chai/workflows/selenium-webdriver-commonjs-mocha-chai/badge.svg)](https://github.com/e2e-boilerplates/selenium-webdriver-commonjs-mocha-chai/actions?workflow=selenium-webdriver-commonjs-mocha-chai)
 
 selenium-webdriver end-to-end test automation boilerplate project. Using commonjs, mocha and chai.
 
---
+++ spec/sandbox.spec.js
@@ -0,0 +1,34 @@
+const { Builder, By } = require("selenium-webdriver");
+const chrome = require("selenium-webdriver/chrome");
+const { expect } = require("chai");
+
+require("chromedriver");
+
+const options = new chrome.Options();
+const chromeOptions = process.env.GITHUB_ACTIONS ? options.headless() : options;
+
+describe("Sandbox", () => {
+  let browser;
+
+  before(async () => {
+    browser = await new Builder()
+      .forBrowser("chrome")
+      .setChromeOptions(chromeOptions)
+      .build();
+    browser.get("https://e2e-boilerplates.github.io/sandbox/");
+  });
+
+  after(() => {
+    browser.quit();
+  });
+
+  it("Should be on Sandbox", async () => {
+    const title = await browser.getTitle();
+    const header = await browser.findElement(By.css("h1"));
+
+    expect(title).to.equal("Sandbox");
+    header.getText().then(text => {
+      expect(text).to.equal("Sandbox");
+    });
+  });
+});
